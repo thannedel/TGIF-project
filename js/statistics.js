247deaa9fadc7c55dc437451f8163639720c9ce6
Thanos Nedelkopoulos <57347092+thannedel@users.noreply.github.com>
2020-11-03 17:35:05 +0100

    Update statistics.js

--- js/statistics.js
+++ js/statistics.js
@@ -58,8 +58,7 @@ function scrollFunction() {
 }
 
 function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+  window.scrollTo({top: 0, behavior: 'smooth'});
 }
 var statistics = {
   numberOfDem: 0,
